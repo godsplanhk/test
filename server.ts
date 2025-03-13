f5e63c93298326fc600cda7d671f15d5372b0d11
Your Name <your_email@example.com>
2025-03-13 18:17:18 +0530

    increase limit

--- server.ts
+++ server.ts
@@ -28,8 +28,8 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit:"50mb" }));
+app.use(express.json({limit:"50mb"}));
 app.use(morgan("combined"));
 
 const limiter = rateLimit({
